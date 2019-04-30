f7b9f9f43ed0ed9a8513c0735f3a632ebef912b2
Ramiz Raja <informramiz@gmail.com>
2019-05-01 00:49:56 +0500

    feat: Try variables and constants

---
+++ Swift-basics.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var str = "Hellow world " + "hi"
+print(str)
+
+var count: Int = 10
+print(count)
+
+var myMiddleName: Character = "A"
+
+let date = "1-2-2004"
+
+//string interpolation
+print("count: \(count)")
