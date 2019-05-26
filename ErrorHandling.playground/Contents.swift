1d3a9d185607634c2ab6f7a3f199d65854f10418
Ramiz Raja <informramiz@gmail.com>
2019-05-26 05:28:56 +0500

    feat: Practice error propagation

--- ErrorHandling.playground/Contents.swift
+++ ErrorHandling.playground/Contents.swift
@@ -15,3 +15,22 @@ func tryErrorGenerator() {
 }
 
 tryErrorGenerator()
+
+
+//this function propogates an error
+func tryErrorThrower() throws {
+    let fileUrl = Bundle.main.bundleURL
+    do {
+        let content = try String(contentsOf: fileUrl, encoding: .utf8)
+        print(content)
+    } catch let error as NSError {
+        print(error)
+        throw NSError(domain: "ErrorTesting", code: 0, userInfo: ["failure": error.localizedDescription])
+    }
+}
+
+do {
+    try tryErrorThrower()
+} catch {
+    print(error)
+}
