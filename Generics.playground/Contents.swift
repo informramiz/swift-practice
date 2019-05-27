6abc3c159d65d5dc76392f38300f39af98afcdf6
Ramiz Raja <informramiz@gmail.com>
2019-05-27 05:25:28 +0500

    feat: Practice generic types

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -21,3 +21,39 @@ func printRaw<T: RawRepresentable>(arg: T) {
 }
 
 printRaw(arg: Position.first)
+
+//generic types
+protocol Animal {
+    var name: String { get }
+    static var commonName: String { get }
+}
+
+struct Whale : Animal {
+    let name: String
+    static var commonName = "Whale"
+}
+
+struct Dolphin: Animal {
+    let name: String
+    static var commonName = "Dolphin"
+}
+
+struct ZooExhibit<AnimalType: Animal> {
+    let animals: [AnimalType]
+    
+    func tourExhibit() {
+        print("Welcome to \(AnimalType.commonName) exhibit")
+        for animal in animals {
+            print("Say hello  to \(animal.name)")
+        }
+    }
+}
+
+let whaleZoo = ZooExhibit(animals: [Whale(name: "Missi sippi"), Whale(name: "Marvan")])
+whaleZoo.tourExhibit()
+print()
+
+let dolphinZoo = ZooExhibit<Dolphin>(animals: [Dolphin(name: "cutie pye"), Dolphin(name: "Charlie")])
+dolphinZoo.tourExhibit()
+
+
