573bee199b941fbfc7b3302adb8178674b81f963
Ramiz Raja <informramiz@gmail.com>
2019-06-03 04:17:05 +0500

    feat: Practice static

--- static.playground/Contents.swift
+++ static.playground/Contents.swift
@@ -4,7 +4,9 @@ import Foundation
 //static property
 //static methods
 class PoliceOfficer {
+    //class/Type property
     static let minimumAllowedAge = 18
+    //instance properties
     var name: String
     var age: Int
     
@@ -13,7 +15,27 @@ class PoliceOfficer {
         self.age = age
     }
     
+    //class method
     static func printMinimumAllowedAge() {
         print("Minimum allowed age for a police officer is: \(minimumAllowedAge)")
     }
 }
+
+//instance/object
+let policeOfficer1 = PoliceOfficer(name: "Ramiz", age: 18)
+let policeOfficer2 = PoliceOfficer(name: "Sep", age: 16)
+
+print(PoliceOfficer.minimumAllowedAge)
+PoliceOfficer.printMinimumAllowedAge()
+
+
+
+
+//--------------------------------//
+
+//Any vs AnyObject
+let a = 10
+let obj: AnyObject = a
+//let primitive: Any = a
+//
+//print(policeOfficer.name)
